4fa7b5a1593e9bd2dcd3b6ff9a3455ec1490d2c5
Mayur Patil <mayurupatil@gmail.com>
2019-09-21 14:10:59 +0530

    Updates

---
+++ precache-manifest.5a6b03fb03f7c3f1f9aa5da96b5e627c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3da6c7d176109b3fcd60cc49339ecbef",
+    "url": "/search-react/index.html"
+  },
+  {
+    "revision": "fbce72446743ec87c88b",
+    "url": "/search-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "447d98d2cf3d25068f60",
+    "url": "/search-react/static/js/2.d0779bf1.chunk.js"
+  },
+  {
+    "revision": "fbce72446743ec87c88b",
+    "url": "/search-react/static/js/main.f79a9965.chunk.js"
+  },
+  {
+    "revision": "c9c0aee38834a2598870",
+    "url": "/search-react/static/js/runtime-main.59a2ec0d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/search-react/static/media/logo.25bf045c.svg"
+  }
+]);
